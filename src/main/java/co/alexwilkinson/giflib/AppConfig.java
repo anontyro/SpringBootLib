8bb560fc831f12e5a37af58fcf4d0ec6e93b27f9
anontyro <alexwilkinson@gmail.com>
2017-05-02 23:52:09 +0800

    Initial commit

---
+++ src/main/java/co/alexwilkinson/giflib/AppConfig.java
@@ -0,0 +1,15 @@
+package co.alexwilkinson.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by Alex on 02/05/2017.
+ */
+@EnableAutoConfiguration
+public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
